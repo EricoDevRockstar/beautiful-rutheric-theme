c1ca0a746cf7fca5b10723c503a55db98020cfd3
Eric W. M <ericmwangi0916@gmail.com>
2022-04-28 23:00:29 +0300

    Add more files including the Stylesheets and other assets

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 
+// Including or Hooking the CSS and JS files to our website.
 function beautiful_rutheric_files() {
-    wp_enqueue_style('beautiful_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('beautiful_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('beautifu_additional_styles', get_theme_file_uri('/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'beautiful_rutheric_files');
