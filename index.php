42b1cd02589ec9ee0846fe362b12b9cafa2daeb4
Eric W. M <ericmwangi0916@gmail.com>
2022-05-11 20:59:56 +0300

    Finished up the Blog page creation.

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <?php get_header(); ?>
 
-<!-- The Banner Section -->
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg'); ?>)"></div>
-      <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">Welcome!</h1>
-        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+ <!-- The Banner Section -->
+ <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title">Welcome to our blog!</h1>
+      <div class="page-banner__intro">
+        <p>Keep up with out latest news.</p>
       </div>
-    </div>
-    
+    </div>  
+  </div>
+
+<div class="container container--narrow page-section">
+  <?php
+    while(have_posts()) {
+      the_post(); ?>
+      <div class="post-item">
+        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <div class="metabox">
+          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n-j-y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+        </div>
+
+        <div class="generic-content">
+          <?php the_excerpt(); ?>
+          <p><a href="<?php the_permalink(); ?>" class="btn btn--blue">Continue reading &raquo;</a></p>
+        </div>
+      </div>
+    <?php }
+    echo paginate_links();
+  ?>
+</div>
    <?php get_footer();
 ?>
