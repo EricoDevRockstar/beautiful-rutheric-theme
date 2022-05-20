aa364e2e683ed1ab704cadb53728a5990b7abcc1
Eric W. M <ericmwangi0916@gmail.com>
2022-05-20 21:19:42 +0300

    Modified file single-professor

--- single-professor.php
+++ single-professor.php
@@ -16,7 +16,7 @@
     </div>
 
     <div class="container container--narrow page-section">
-        
+
         <div class="generic-content">
             <?php the_content(); ?>
         </div>
@@ -27,7 +27,7 @@
 
         if ($beautifulRelatedPrograms) {
           echo '<hr class="section-break">';
-          echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+          echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
           echo '<ul class="link-list min-list">';
           foreach($beautifulRelatedPrograms as $program) { ?>
             <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
