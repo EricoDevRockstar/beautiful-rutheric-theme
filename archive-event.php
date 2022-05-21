f606098d571971de8113d0fe0cd8560366cf776d
Eric W. M <ericmwangi0916@gmail.com>
2022-05-22 02:06:54 +0300

    Modified files accordingly

--- archive-event.php
+++ archive-event.php
@@ -1,15 +1,9 @@
-<?php get_header(); ?>
-
- <!-- The Banner Section -->
- <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">All Events</h1>
-      <div class="page-banner__intro">
-        <p>See what events are upcoming in out world!</p>
-      </div>
-    </div>  
-  </div>
+<?php get_header();
+beautifulPageBanner(array(
+  'title' => 'All Events',
+  'subtitle' => 'See what events are upcoming in out world!'
+));
+?>
 
 <div class="container container--narrow page-section">
   <?php
