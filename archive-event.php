c1aa0cc0589ef1c7159255ff9b17d1cdf5a5a48c
Eric W. M <ericmwangi0916@gmail.com>
2022-05-22 02:37:29 +0300

    Created new folder and files plus other files modification

--- archive-event.php
+++ archive-event.php
@@ -8,27 +8,9 @@ beautifulPageBanner(array(
 <div class="container container--narrow page-section">
   <?php
     while(have_posts()) {
-      the_post(); ?>
-
-                <div class="event-summary">
-                  <a class="event-summary__date t-center" href="#">
-                        <span class="event-summary__month">
-
-                        <?php
-                        $beautifulDate = new DateTime(get_field('event_date'));
-                        echo $beautifulDate->format('M');
-                        ?>
-
-                        </span>
-                        <span class="event-summary__day"><?php echo $beautifulDate->format('d');?></span>
-                    </a>
-                  <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
-                  </div>
-                </div>
-
-    <?php }
+      the_post();
+      get_template_part('template-parts/event');
+    }
     echo paginate_links();
   ?>
 
--- front-page.php
+++ front-page.php
@@ -37,34 +37,9 @@
 
             while($beautifulEvents->have_posts()) {
 
-            $beautifulEvents->the_post(); ?>
-
-            <div class="event-summary">
-              <a class="event-summary__date t-center" href="#">
-                <span class="event-summary__month">
-
-                <?php
-                  $beautifulDate = new DateTime(get_field('event_date'));
-                  echo $beautifulDate->format('M');
-                ?>
-
-                </span>
-                <span class="event-summary__day"><?php echo $beautifulDate->format('d');?></span>
-              </a>
-              <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p>
-                <?php if (has_excerpt()) {
-                    echo get_the_excerpt();
-                    } else {
-                    echo wp_trim_words(get_the_content(), 18);
-                    }
-                ?>  
-                <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-              </div>
-            </div>
-
-          <?php }
+            $beautifulEvents->the_post();
+            get_template_part('template-parts/event');
+          }
 
           ?>
           <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
