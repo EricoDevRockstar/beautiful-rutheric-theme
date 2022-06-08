e0ee2d81e35d01449a0c2edec5c9670ee7f451b4
Eric W. M <ericmwangi0916@gmail.com>
2022-06-09 02:26:14 +0300

    Modification made

--- page-my-notes.php
+++ page-my-notes.php
@@ -14,7 +14,25 @@ if (!is_user_logged_in()) {
 
   <div class="container container--narrow page-section">
 
-  Custom code for my notes will go here.
+    <ul class="min-list link-list" id="my-notes">
+        <?php
+            $userBeautifulNotes = new WP_Query(array(
+                'post_type' => 'note',
+                'posts_per_page' => -1,
+                'author' => get_current_user_id()
+            ));
+
+            while($userBeautifulNotes->have_posts()) {
+                $userBeautifulNotes->the_post(); ?>
+                <li>
+                    <input class="note-title-field" type="text" value="<?php echo esc_attr(get_the_title()); ?>">
+                    <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+                    <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
+                    <textarea class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                </li>
+            <?php }
+        ?>
+    </ul>
 
   </div> <!-- The content starts here End -->
     
